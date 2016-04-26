b5e8d5d2698b62d1bdd22f8a070bd6cb380edcfe
Olivier Guiter <oguiter@free.fr>
2016-04-26 20:36:54 +0200

    first commit

---
+++ sensors_app.go
@@ -0,0 +1,318 @@
+// --------------------------------------------
+//
+//
+// --------------------------------------------
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"text/template"
+	"time"
+
+	"golang.org/x/net/context"
+	"google.golang.org/cloud/pubsub"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/cloud"
+	"io/ioutil"
+
+	"container/list"
+)
+
+// Estimote data struct
+type GpsReadout struct {
+	Latitude  float64 `json:"lat,string"`
+	LatPole   string  "latPole"
+	Longitude float64 `json:"lon,string"`
+	LonPole   string  "lonPole"
+}
+
+type TlmData struct {
+	Version uint8   "version"
+	Vbatt   uint16  "vbatt"
+	Temp    float32 "temp"
+	AdvCnt  uint32  "advCnt"
+	SecCnt  uint32  "secCnt"
+}
+
+type Estimote struct {
+	TagId             string     "tagId"
+	GatewayId         string     "gatewayId"
+	GatewayLocation   string     "gatewayLocation"
+	GatewayGpsReadout GpsReadout "gatewayGpsReadout"
+	LastSeen          int64      "lastSeen"
+	SmoothingWindow   int        "smoothingWindow"
+	LowerDistance     float64    "lowerDistance"
+	MeanDistance      float64    "meanDistance"
+	UpperDistance     float64    "upperDistance"
+
+	Tlm 		  TlmData "tlm"
+}
+
+type TagEntry struct {
+	TagId    string
+	Count    int
+	DataList *list.List // Store the last MaxDataKeep (100)
+}
+
+type PieData struct {
+	Title      string
+	ChartType  string
+	SubTitle   string
+	SeriesName string
+	DataArray  string
+}
+
+var (
+	count   int
+	countMu sync.Mutex
+	ctx     context.Context
+
+	TagsListMu sync.Mutex
+	TagsList   []TagEntry
+)
+
+const (
+	ProjID      = "sensor-it"   // could be get from os.Getenv[GCLOUD_PROJECT")
+	SubTopic    = "sensorTopic" // could ... PUBSUB_TOPIC
+	SubName     = "sensorSub"   // could ... PUBSUB_SUB
+	MaxDataKeep = 100
+
+	KeyFileJSON = "sensor-it-7021d8976ff0.json"
+)
+
+func main() {
+
+	// Get the context
+	ctx := cloudContext(ProjID)
+
+	// a new client ...
+	client, err := pubsub.NewClient(ctx, ProjID)
+	if err != nil {
+		log.Fatalf("creating pubsub client: %v", err)
+	}
+
+	// Create topic (check if exist...)
+	exists, err := client.Topic(SubTopic).Exists(ctx)
+	if err != nil {
+		log.Fatalf("Checking topic exists failed: %v", err)
+	}
+	if exists {
+		fmt.Printf("Topic %s is already created.\n", SubTopic)
+	} else {
+		_, err = client.NewTopic(ctx, SubTopic)
+		if err != nil {
+			log.Fatalf("Creating topic failed: %v", err)
+		}
+		fmt.Printf("Topic %s was created.\n", SubTopic)
+	}
+
+	// Create subscription
+	exists, err = client.Subscription(SubName).Exists(ctx)
+	if err != nil {
+		log.Fatalf("Checking subscription exists failed: %v", err)
+	}
+
+	if exists {
+		fmt.Printf("Subscription %s is already there.\n", SubName)
+	} else {
+		_, err = client.NewSubscription(ctx, SubName, client.Topic(SubTopic), time.Duration(0), nil)
+
+		if err != nil {
+			log.Fatalf("Creating Subscription failed: %v", err)
+		}
+		fmt.Printf("Subscription %s was created.\n", SubName)
+	}
+
+	/* handler dispatcher */
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/_ah/health", healthCheckHandler)
+	http.HandleFunc("/_ah/stop", shutdownHandler)
+	http.HandleFunc("/pie", HandlerPIE)
+	http.HandleFunc("/test", HandlerTEST)
+
+	go subscribe(ctx)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+/* ---------------------------------------------
+ * Thread sub: Wait for incoming messages from sensors
+ * thru the pubsub channel
+ */
+func subscribe(ctx context.Context) {
+	log.Printf("In subscribe (ctx: %+v)!!!\n", ctx)
+	for {
+		// Pull up to 10 messages (maybe fewer) from the subscription.
+		// Blocks for an indeterminate amount of time.
+		msgs, err := pubsub.PullWait(ctx, SubName, 10)
+		if err != nil {
+			log.Fatalf("could not pull: %v", err)
+		}
+
+		for _, m := range msgs {
+			msg := m
+
+			var estimote Estimote
+
+			if err := json.Unmarshal(msg.Data, &estimote); err != nil {
+				log.Printf("could not decode message data: %s \n%v", msg.Data, err)
+				go pubsub.Ack(ctx, SubName, msg.AckID)
+				continue
+			} else {
+			//log.Printf("Debug data: %s\n", msg.Data)
+			log.Printf("Debug gw loc: %#v\n\n", estimote)
+			}
+
+			//		log.Printf("[ID %s] Processing.", estimote.TagId)
+			go func() {
+				if err := updateData(&estimote); err != nil {
+					log.Printf("[ID %d] could not update: %v", estimote.TagId, err)
+					return
+				}
+
+				countMu.Lock()
+				count++
+				countMu.Unlock()
+
+				pubsub.Ack(ctx, SubName, msg.AckID)
+				//log.Printf("[ID %d] ACK", estimote.TagId)
+			}()
+		}
+	}
+
+}
+
+/* Update the Sensors array
+ * count, for each sensor, the number of incoming frames
+ * and, in another array, store the last 10 records
+ */
+func updateData(estimote *Estimote) error {
+	//log.Printf("UPDATE [ID %s]\n", estimote.TagId)
+	var bfound bool
+
+	bfound = false
+	for idx := range TagsList {
+		if TagsList[idx].TagId == estimote.TagId {
+			//log.Printf("Found [ID %s]: %d \n", estimote.TagId, TagsList[idx].Count)
+		TagsListMu.Lock()
+			TagsList[idx].Count++
+			TagsList[idx].DataList.PushFront(estimote)
+
+			if TagsList[idx].DataList.Len() > MaxDataKeep {
+				TagsList[idx].DataList.Remove(TagsList[idx].DataList.Back())
+			//	log.Printf("Queue Len [ID %s]: %d\n", estimote.TagId, TagsList[idx].DataList.Len())
+			}
+		TagsListMu.Unlock()
+			bfound = true
+		}
+	}
+	// if not found, add the entry
+	if bfound == false {
+		var esttmp TagEntry
+		esttmp.TagId = estimote.TagId
+		esttmp.Count = 0
+		//Create the data ring buffer
+		esttmp.DataList = list.New()
+		esttmp.DataList.PushFront(estimote)
+		TagsListMu.Lock()
+		TagsList = append(TagsList, esttmp)
+		TagsListMu.Unlock()
+		log.Printf("Append to list [ID %s]\n", estimote.TagId)
+		fmt.Println("%#v",estimote)
+	}
+
+	return nil
+}
+
+/*
+ */
+func HandlerTEST(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Shutdown app\n")
+	for i := range TagsList {
+		fmt.Println("-----------------------")
+		e := TagsList[i].DataList.Front()
+			// do something with e.Value
+		fmt.Println("%#v", e.Value)
+		fmt.Println("%+v", e.Value)
+		fmt.Println("%T", e.Value)
+		}
+}
+
+/*
+ */
+func shutdownHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Shutdown app\n")
+}
+
+/*
+ */
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	TagsListMu.Lock()
+	defer TagsListMu.Unlock()
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	if err := tmpl.Execute(w, TagsList); err != nil {
+		log.Printf("Could not execute template: %v", err)
+	}
+}
+
+/*
+ */
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
+
+/*
+ */
+func cloudContext(projectID string) context.Context {
+
+	// Get the key file
+	jsonKey, err := ioutil.ReadFile(KeyFileJSON)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conf, err := google.JWTConfigFromJSON(
+		jsonKey,
+		pubsub.ScopeCloudPlatform,
+		pubsub.ScopePubSub,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := cloud.NewContext(projectID, conf.Client(oauth2.NoContext))
+
+	return ctx
+
+}
+
+// Very simple template to display sensors various data
+var tmpl = template.Must(template.New("").Parse(`<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sensor IT</title>
+  </head>
+  <body>
+    <div>
+      <p>Tag ID list received by this instance:</p>
+      <ul>
+    	{{range .}}
+<li>Tag ID: {{.TagId}} / msgs received: {{.Count}}</li>
+    	{{end}}
+      </ul>
+    </div>
+    <p>Note: if the application is running across multiple instances, each
+      instance will have its own list of tag ID.</p>
+  </body>
+</html>`))
+
+//Eof
---
+++ sensors_pie.go
@@ -0,0 +1,122 @@
+// --------------------------------------------
+//
+// --------------------------------------------
+
+package main
+
+import (
+//	"encoding/json"
+//	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+//	"sync"
+	"text/template"
+//	"time"
+
+//	"golang.org/x/net/context"
+//	"google.golang.org/cloud/pubsub"
+
+//	"golang.org/x/oauth2"
+//	"golang.org/x/oauth2/google"
+//	"google.golang.org/cloud"
+//	"io/ioutil"
+)
+
+
+/*---------------------
+ */
+func HandlerPIE(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/pie" {
+		http.NotFound(w, r)
+		return
+	}
+
+	var pp PieData
+	pp.ChartType = "pie"
+	pp.Title = "Sensors activities 2016"
+	pp.SubTitle = "GCP sample test"
+	pp.SeriesName = "Sensors"
+
+	// Prepare the data array (label/value)
+	//[[lab1, val1],[lab2, val2],...]  !!! text/html
+	pp.DataArray = "["
+	for i := range TagsList {
+		pp.DataArray = pp.DataArray + "['" + TagsList[i].TagId + "' , " + strconv.Itoa(TagsList[i].Count) + "],"
+	}
+	pp.DataArray = pp.DataArray + "]"
+
+	if t, err := template.New("foo").Parse(TemplatePieHtml); err != nil {
+		log.Printf("Could not create template: %v", err)
+	} else {
+		if err = t.ExecuteTemplate(w, "T", pp); err != nil {
+			log.Printf("Could not execute template: %v", err)
+		}
+	}
+}
+
+// .............
+var TemplatePieHtml = `{{define "T"}}
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>GCP Sample - {{.ChartType}}</title>
+
+        <script type="text/javascript" src="http://cdn.hcharts.cn/jquery/jquery-1.8.3.min.js"></script>
+ <script type="text/javascript">
+        $(function () {
+            $('#container').highcharts({
+                chart: {
+                   // type: 'pie',
+                    type: '{{.ChartType}}',
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false
+                },
+                title: {
+                    text: '{{.Title}}',
+                },
+                subtitle: {
+                    text: '{{.SubTitle}}',
+                },
+                tooltip: {
+                    /*pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'*/
+                    pointFormat: '{series.name}: <b>{point.y}</b><br/>',
+              },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                            style: {
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            }
+                        }
+                    }
+                },
+                series: [{
+                    data : {{.DataArray}}
+                }]
+            });
+        });
+		</script>
+    </head>
+ 
+    <body>
+    By <a id="copyright" class="anchor" href="http://www.intel.com" >olivier.guiter@intel.com</a>
+        
+    <script type="text/javascript" src="http://cdn.hcharts.cn/highcharts/4.2.4/highcharts.js"></script>
+    <script type="text/javascript" src="http://cdn.hcharts.cn/highcharts/4.2.4/modules/exporting.js"></script>
+
+    <div id="container" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
+
+    </body>
+</html>
+{{end}}
+`
+
+//Eof
